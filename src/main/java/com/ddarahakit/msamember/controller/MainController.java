1fd9a25c6945741be17e1813edadb4b4471acd43
ddarahakit <ddarahakit@gmail.com>
2022-05-17 14:51:27 +0900

    젠킨스 테스트

---
+++ src/main/java/com/ddarahakit/msamember/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.ddarahakit.msamember.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MainController {
+    @GetMapping("/test")
+    public String test(){
+        return "test";
+    }
+}

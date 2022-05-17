4fce7d683b5dd6517793403714cacb7cdabf7e06
ddarahakit <ddarahakit@gmail.com>
2022-05-17 12:00:42 +0900

    init

---
+++ src/main/java/com/ddarahakit/msamember/MsaMemberApplication.java
@@ -0,0 +1,13 @@
+package com.ddarahakit.msamember;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaMemberApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaMemberApplication.class, args);
+    }
+
+}
